97c420e67b3f5be5c133b270b1d724edc7bc70fe
Renato Affonso <106754808+RenatoAffonsoX@users.noreply.github.com>
2022-10-13 00:29:26 -0300

    Add files via upload

---
+++ Desafio-DIO-Pottencial-Hospedagem/Models/Reserva.cs
@@ -0,0 +1,61 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Reserva(Suite suite)
+        {
+            this.Suite = suite;
+
+        }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Número de hospedes superior a capacidade da Suite.");
+
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            decimal valor = 0;
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 10 / 100;
+            }
+
+            return valor;
+        }
+    }
+}
